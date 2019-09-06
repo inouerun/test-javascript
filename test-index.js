33bcf530ba724fd308899b153b09c9e9ca421bae
r_inoue <r_inoue@IT-PC-1904-0307.local>
2019-09-06 11:09:49 +0900

    arrow functin writing

---
+++ test-index.js
@@ -0,0 +1,13 @@
+var elements = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+const result = elements.map(function(element){
+  return element.length;
+});
+
+
+console.log(result);
